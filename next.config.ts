2e3ab45913167034b5d13ed6f5db81fce6ce8992
Dipan Mazumder <dipanmajumder55@gmail.com>
2026-06-04 21:17:55 +0530

    Update next.config.ts with new settings
    Add output and images configuration to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   allowedDevOrigins: ["192.168.29.185"],
-  /* config options here */
+  allowedDevOrigins: ["192.168.29.185"],
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
